24a43731a3b8365309184d44778f96bac383ae84
Gus Schissler <gus@gwydion.co>
2018-02-14 19:54:15 -0500

    recommit, add new strata video, finishing touches

--- js/video-scroll.js
+++ js/video-scroll.js
@@ -3,7 +3,7 @@ var vid = document.getElementById('strata-video');
 var container = $('#main-info').height();
 var time = $('#time');
 var scroll = $('#scroll');
-var windowheight = $(window).height();
+var windowheight = $(window).height()-20;
 
 
 var scrollpos = window.pageYOffset/200;
@@ -12,15 +12,16 @@ var accel = 0;
 
 
 // ---- Values you can tweak: ----
-var accelamount = 0.1; //How fast the video will try to catch up with the target position. 1 = instantaneous, 0 = do nothing.
+var accelamount = 0.86; //How fast the video will try to catch up with the target position. 1 = instantaneous, 0 = do nothing.
+var bounceamount = 0.01; //value from 0 to 1 for how much backlash back and forth you want in the easing. 0 = no bounce whatsoever, 1 = lots and lots of bounce
 
 // pause video on load
 vid.pause();
 
-
 window.onscroll = function(){
     //Set the video position that we want to end up at:
-    targetscrollpos = Math.floor(window.pageYOffset/200);
+    targetscrollpos = window.pageYOffset/200;
+
     //move the red dot to a position across the side of the screen
     //that indicates how far we've scrolled.
     scroll.css('top', 10+(window.pageYOffset/container*windowheight));
@@ -30,8 +31,15 @@ window.onscroll = function(){
 setInterval(function(){
 
       //Accelerate towards the target:
-      scrollpos += (targetscrollpos - scrollpos)*accelamount;
-      console.log(scrollpos);
+      accel += (targetscrollpos - scrollpos)*accelamount;
+
+      //clamp the acceleration so that it doesnt go too fast
+      if (accel > 1) accel = 1;
+      if (accel < -1) accel = -1;
+
+      //move the video scroll position according to the acceleration and how much bouncing you selected:
+      scrollpos = (scrollpos + accel) * (bounceamount) + (targetscrollpos * (1-bounceamount));
+
       //move the blue dot to a position across the side of the screen
       //that indicates where the current video scroll pos is.
       time.css('top', 10+(scrollpos/container*200*windowheight));
